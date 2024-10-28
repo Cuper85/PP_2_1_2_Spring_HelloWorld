b2555dd234474f4b8c74f0718099634ef7f986ab
Burtsev_Sergey <burtsev-sa@yandex.ru>
2024-10-28 15:29:40 +0300

    It's ok

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,28 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorldBean1 =
+                (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
+
+        HelloWorld helloWorldBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
+
+        System.out.println("Переменные HelloWorld ссылаются на один и тот же объект? " + (helloWorldBean1 == helloWorldBean2));
+
+        Cat catBean1 =
+                (Cat) applicationContext.getBean("cat", Cat.class);
+
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Переменные Cat ссылаются на один и тот же объект? " + (catBean1 == catBean2));
     }
 }
+
+
